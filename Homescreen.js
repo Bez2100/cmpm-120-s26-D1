a1b5fa7a8550054b83dc32e5836a066c0e138dd3
Bez2100 <jlbaeza@ucsc.edu>
2026-04-17 19:12:32 -0700

    added phaser and updated html file name

---
+++ Homescreen.js
@@ -0,0 +1,17 @@
+class Homescreen extends Phaser.Scene {
+    constructor() {
+        super('homescreen');
+    }
+
+    preload() {
+        // Load assets for the homescreen
+    }
+
+    create() {
+        // Create homescreen elements
+    }
+
+    update() {
+        // Update homescreen elements
+    }
+}
