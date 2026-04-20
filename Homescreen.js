8a6d991c972e405b4abe69d37ce06b74fa2a4019
Bez2100 <jlbaeza@ucsc.edu>
2026-04-19 21:49:27 -0700

    Created the main game menu screen

--- Homescreen.js
+++ Homescreen.js
@@ -4,11 +4,35 @@ class Homescreen extends Phaser.Scene {
     }
 
     preload() {
-        // Load assets for the homescreen
+        this.load.image('homeBackground', 'assets/background.jpg');
     }
 
     create() {
-        // Create homescreen elements
+        this.add.image(this.scale.width / 2, this.scale.height / 2, 'homeBackground')
+            .setDisplaySize(this.scale.width, this.scale.height);
+
+        this.add.text(this.scale.width / 2, 200, "Welcome to Fallen!", {
+            fontFamily: '"Press Start 2P"',
+            fontSize: '32px',
+            color: '#9dfc8b',
+            stroke: '#102030',
+            strokeThickness: 6,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.add.text(this.scale.width / 2, 300, "Press SPACE to Start", {
+            fontFamily: '"Press Start 2P"',
+            fontSize: '20px',
+            color: '#9dfc8b',
+            stroke: '#102030',
+            strokeThickness: 4,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.input.keyboard.on('keydown-SPACE', () => {
+            this.scene.start('game');
+        });
+
     }
 
     update() {
